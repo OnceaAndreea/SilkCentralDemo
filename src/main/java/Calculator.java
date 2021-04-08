5ac130aa2ade6b3139c5b1b48fab3f317216ed06
OnceaAndreea <kH9zUXmE>
2021-04-08 11:30:59 +0300

    created tests

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+    public int multimply(int a, int b) {
+        return a*b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+
+        Assert.assertEquals(5, calculator.sum(2, 3));
+    }
+
+    @Test
+    public void testMultimply() {
+        Assert.assertEquals(6, calculator.multimply(2, 3));
+    }
+
+}
