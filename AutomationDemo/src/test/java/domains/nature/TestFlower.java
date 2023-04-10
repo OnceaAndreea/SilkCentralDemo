45cb5ae99af2b1077a47935f2108bb5f48c5aafa
OnceaAndreea <andreea.oncea@microfocus.com>
2023-04-10 14:08:22 +0300

    added flower test

---
+++ AutomationDemo/src/test/java/domains/nature/TestFlower.java
@@ -0,0 +1,16 @@
+package domains.nature;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestFlower {
+
+    Flower flower = new Flower("Hortensia", true, "blue");
+
+    @Test
+    public void checkFlowerName() {
+        assertEquals(flower.getName(), "Hortensia");
+    }
+
+}
