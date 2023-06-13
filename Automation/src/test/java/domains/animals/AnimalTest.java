e46828581c2e7758d342601ea46ca0f84ae56329
AnAdam <andrei.adam@microfocus.com>
2023-06-13 15:11:30 +0300

    add exec folder

--- Automation/src/test/java/domains/animals/AnimalTest.java
+++ Automation/src/test/java/domains/animals/AnimalTest.java
@@ -22,4 +22,21 @@ public class AnimalTest {
         System.out.println(System.getProperty("param1"));
         System.out.println("aaaaaaaaaaa");
     }
+
+    @Test
+    public void showParams() {
+        System.out.println(System.getProperty("#sctm_execdef_name"));
+        System.out.println(System.getProperty("#sctm_execdef_id"));
+        System.out.println(System.getProperty("#sctm_product"));
+        System.out.println(System.getProperty("#sctm_version"));
+        System.out.println(System.getProperty("#sctm_build"));
+        System.out.println(System.getProperty("#sctm_keywords"));
+        System.out.println(System.getProperty("#sctm_regular_execdef_run_id"));
+        System.out.println(System.getProperty("#sctm_test_name"));
+        System.out.println(System.getProperty("#sctm_test_id"));
+        System.out.println(System.getProperty("#sctm_source_root_dir"));
+        System.out.println(System.getProperty("#external_id"));
+        System.out.println(System.getProperty("#sctm_data_driven_parent_test_name"));
+        System.out.println(System.getProperty("#sctm_data_driven_parent_test_id"));
+    }
 }
